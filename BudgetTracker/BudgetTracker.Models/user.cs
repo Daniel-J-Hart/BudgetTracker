29b5d305679c2c2d1b8ba1504e149ced05688aee
Daniel Hart <danielthehart@outlook.com>
2025-05-20 17:01:12 +0100

    Fix warnings and cleanup

--- BudgetTracker/BudgetTracker.Models/user.cs
+++
@@ -1,8 +0,0 @@
-namespace BudgetTracker.Models
-{
-    public class User
-    {
-        public string Username { get; set; }
-        public string PasswordHash { get; set; }
-    }
-}
--- obj/Debug/net9.0/BudgetTracker.AssemblyInfo.cs
+++ obj/Debug/net9.0/BudgetTracker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BudgetTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdaf8ee2f70f05d14b95b716926e70f577d9adbd")]
 [assembly: System.Reflection.AssemblyProductAttribute("BudgetTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BudgetTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
